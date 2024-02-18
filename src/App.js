d56394f431d935f07fc0708b83ba9dc690979231
Dave Suico <suico.dave@gmail.com>
2024-02-18 08:53:51 +0800

    display expenses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+ function renderExpenses(expenses) {
+     const rows = expenses.map((expense) => {
+       return (
+         <tr key={expense._id}>
+           <td>{expense._id}</td>
+           <td>{expense.description}</td>
+           <td>{expense.amount}</td>
+          <td>{new Date(expense.date).toDateString()}</td>
+        </tr>
+       );
+    });
+  
+    return rows;
+   }
+
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  const fetchExpenses = async () => {
+    const apiUrl = "https://workshop-expense-api-dave.onrender.com";
+
+    const endpoint = `${apiUrl}/api/expenses`;
+
+    const response = await fetch(endpoint);
+
+    const expenseData = await response.json();
+
+    setExpenses(expenseData);
+  };
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
   return (
     <div>
       <form>
@@ -23,7 +57,9 @@ function App() {
           </tr>
         </thead>
 
-        <tbody></tbody>
+        <tbody>
+        {renderExpenses(expenses)}
+        </tbody>
       </table>
     </div>
   );
